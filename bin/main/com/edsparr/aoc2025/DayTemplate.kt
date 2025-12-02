30c14d4c23ee01d4429e7e54813875859e4c7832
Henrik Edsparr <henrik.edsparr@gmail.com>
2025-12-02 20:48:25 +0100

    Solution for Day 1 and 2

--- bin/main/com/edsparr/aoc2025/DayTemplate.kt
+++
@@ -1,8 +0,0 @@
-package com.edsparr.aoc2025
-
-class DayX(private val input: List<String>) {
-
-    fun solvePart1(): Int = 0
-
-    fun solvePart2(): Int = 0
-}
--- bin/main/com/edsparr/aoc2025/Main.kt
+++
@@ -1,8 +0,0 @@
-package com.edsparr.aoc2025
-
-fun main() {
-    val input = Resources.resourceAsListOfString("Day01.txt")
-    val day01 = Day01(input)
-    println("Part 1: ${day01.solvePart1()}")
-    println("Part 2: ${day01.solvePart2()}")
-}
